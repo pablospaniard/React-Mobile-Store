050a1673e201591bf3c8b25a031a6f7cd2e2bae3
Pavel Ivanov <pablospaniard@gmail.com>
2018-01-07 16:59:55 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './main.css';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {Router, Route} from 'react-router';
+
+import reducers from 'reducers';
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+import Phone from 'containers/phone';
+import Basket from 'containers/basket';
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDom.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+                <Route path='/categories/:id' component={Phones}/>
+            </Route>
+            <Route path='/phones/:id' component={Phone} />
+            <Route path='/basket' component={Basket} />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
